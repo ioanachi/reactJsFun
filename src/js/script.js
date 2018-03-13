e19701856d458bc5bf3c9005cfe0a1ce7b84a2c1
ioanachi <ioana_chichi@outlook.com>
2018-03-13 16:35:24 +0200

    first4 tutorials

---
+++ src/js/script.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+class BAcon extends React.Component {
+    render() {
+        return (<div>
+            <h3>Tis</h3>
+            <p>Runnnnnnnnnnn</p>
+        </div>);
+    }
+};
+
+
+ReactDOM.render(<div><BAcon /><BAcon /><BAcon /></div>, document.getElementById('example'));
